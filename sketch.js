fa9efb246d74403c7ad38fb34712a86ad9bc472d
adityakharga <72646575+adityakharga@users.noreply.github.com>
2020-10-10 17:23:14 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -70,7 +70,7 @@ function setup() {
   cloudsGroup = createGroup();
 
   
-  trex.setCollider("rectangle",0,0,trex.width,trex.height);
+  trex.setCollider("rectangle",0,0,70,70);
   trex.debug = false
   
   score = 0;
